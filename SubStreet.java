60b715b5f958e14086f9982a5949894031bce274
FerasSaeed <104095419+FerasSaeed@users.noreply.github.com>
2022-05-24 20:37:29 +0300

    Update SubStreet.java

--- SubStreet.java
+++ SubStreet.java
@@ -1,6 +1,8 @@
 package clock;
 import java.util.ArrayList;
 public class SubStreet {
+	// Main street name
+	private String mainS;
 	// Sub Street name
 	private String name;
 	// all adjacent sub streets
@@ -8,17 +10,20 @@ public class SubStreet {
 	// length of sub street
 	public int length;
 	// Traffic percent on each sub street
-	public SubStreet(String name, int length) {
+	public SubStreet(String mainS,String name, int length) {
 		this.name = name;
 		this.length = length;
+		this.mainS = mainS;
 	}
 	// add adjacent sub street
 	public void addSubStreet(SubStreet s) {
 		adjacentSubStreets.add(s);
 	}
-
 	public String getName() {
 		return name;
+	}
+	public String getMainName() {
+		return mainS;
 	}
 	public double getTrafficPercent() {
 		return 1 + Math.random()*0.5;	
