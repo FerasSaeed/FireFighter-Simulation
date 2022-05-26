03027d793a4c6c2a9041affec050c93861ef1c7a
abdulqaders <105943936+abdulqaders@users.noreply.github.com>
2022-05-26 04:26:30 +0300

    Update TrappedTruck.java

--- TrappedTruck.java
+++ TrappedTruck.java
@@ -4,13 +4,11 @@ public class TrappedTruck extends FireStationTruck {
 	private int freeTrappedDuration; // time to release people.
 	
 	
-	public TrappedTruck( Incident incident, SubStreet location, Route route) {
-		//here generate route between incident and fire truck location
-				route.makeRoute(location, incident.getLocation());
+	public TrappedTruck( Incident incident, Route route) {
 		//based on it make arrival time
 				setArrivalTime(route, incident);
 		// here we pass number of trapped, look into Incident class to see the number, based on it decide the duration, pass it to get the mission end time		
-				getMissionEndTime(getArrivalTime(), setFreeTrappedDuration(incident.getTrappedNum()));
+				setMissionEndTime(getArrivalTime(), getFreeTrappedDuration(incident.getTrappedNum()));
 				
 	}
 	
@@ -18,16 +16,27 @@ public class TrappedTruck extends FireStationTruck {
 	
 //set the arrivalTime to the incident location.
 	public void setArrivalTime(Route route, Incident incident) {
-		arrivalTime= route.getLength(); 
-		arrivalTime= new Clock(arrivalTime.getHour(),arrivalTime.getMinute());
-		departureTime = incident.getTimeOfFire();
-		departureTime= new Clock(TimeOfFire.getHour(), TimeOfFire.getMinute());
-		departureTime.addTime(0, 2);
+
+		/* from route.getLength(), you get the distance to incident location, calculate the duration, change from minute To hours and minute
+		then add it to departureTime.*/
+		
+		departureTime= new Clock(incident.getTimeOfFire.getHour(), incident.getTimeOfFire.getMinute()); //convert to (hh:mm)
+		departureTime.addTime(0, 2); //add 2 minutes
+		int velocity= 120/60; // convert the truck speed from hours -> minutes.
+		double timeToIncident= route.getLength() / velocity;
+		
+		int hours = (int) (timeToIncident / 60); 
+		int minutes = (int) (timeToIncident % 60);  
+	
+	//change the departure time to (hh:mm) then add it to timeToIncident.	
+		arrivalTime= new Clock(departureTime.getHour(), departureTime.getMinute());
+		arrivalTime.addTime(hours, minutes);	
 	}
 	
 	
 	
-	public int setFreeTrappedDuration(int trappedNum) {
+// get the duration of releasing people from fire location
+	public int getFreeTrappedDuration(int trappedNum) {
 	//generate random numbers for each case.
 		if(trappedNum <=8)
 			return freeTrappedDuration= (int) (Math.random()*(30-15+1)+15);
@@ -41,7 +50,7 @@ public class TrappedTruck extends FireStationTruck {
 	
 	
 //compute the arrivalTime + FreeTrappedDuration   
-	public Clock getMissionEndTime(Clock arrivalTime, int FreeTrappedDuration) {
+	public void setMissionEndTime(Clock arrivalTime, int FreeTrappedDuration) {
 		
 		int hours = (int) (this.freeTrappedDuration / 60); //since both are ints, you get an int
 		int minutes = (int) (this.freeTrappedDuration % 60);  
@@ -50,13 +59,36 @@ public class TrappedTruck extends FireStationTruck {
 		 missionEndTime = new Clock(arrivalTime.getHour(), arrivalTime.getMinute());
 		 missionEndTime.addTime(hours, minutes);		 
 		
-		 return missionEndTime;
+		 
+	}
+	
+	
+	// get the total time of the mission.
+		public Clock getMissionEndTime() {
+			return missionEndTime;
+	}
+	
+	
+	// get the arrivalTime to the incident location.
+		public Clock getArrivalTime() { 
+			return arrivalTime;
 	}
 		
+		
+
 }
+
+
+
 
 	
 	
+
+
+
+
+
+
 
 
 
