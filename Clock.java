07b8eee50c2edf4ca2e31191ea6d66d2e105d0da
Mzuayr <105936613+Mzuayr@users.noreply.github.com>
2022-06-03 02:30:40 +0300

    Update Clock.java

--- Clock.java
+++ Clock.java
@@ -1,143 +1,175 @@
-package clock;
-
+package FireFighter;
+/**
+ *  This is a class to deal with time data
+ * @author Mohammed Khalid Zuayr
+ *
+ */
 public class Clock {
 
-private int hour;
-private int minute;
-
-public Clock(int h, int m) {
+	/**
+	 * Time in hours
+	 */
+	private int hour;
 	
-	hour = h;
-	minute = m;
-	
-	
-}	
+	/**
+	 * Time in minutes
+	 */
+	private int minute;
+/**
+ * Initialize time
+ * @param h time in hours
+ * @param m time in minutes
+ */
+	public Clock(int h, int m) {
 
-public void setHour(int h) {
-	hour = h;
-	
-}
+		hour = h;
+		minute = m;
 
-public void setMinute(int m) {
-	minute = m;
-	
-}
-
-
-public int getHour() {
-	return hour;
-	
-}
-
-
-public int getMinute() {
-	return minute;
-}
-
-
-public void addTime(int h, int m) {
-	
-	hour = hour + h;
-	//**after that make sure if hour is bigger than 23 then day + 1, and hour gets reset to the 24 hours formula
-    if (hour > 23) {
-    	hour = hour - hour;
-    }
-	
-	
-	minute = minute + m;
-	//** after that make sure if minute is bigger than 59 then hour + 1, and minutes gets reset to the 60 minutes formula
-	if (minute > 59) {
-		hour = hour + 1;
-		minute = minute - minute;
 	}
-}
+/**
+ * to change hours
+ * @param h time in hours
+ */
+	public void setHour(int h) {
+		hour = h;
 
-
-public void substractTime(int h, int m) {
-	hour = hour - h;
-	if (hour < 0) {
-		hour = hour + 24;
 	}
-	
-	minute = minute - m;
-	if(minute < 0) {
-		hour = hour - 1;
-		minute = minute + 60;
-	}
-	
-}
+/**
+ * to change minutes
+ * @param m time in minutes
+ */
+	public void setMinute(int m) {
+		minute = m;
 
-
-
-public String getCurrentTime() {
-
-	// **needs an if or switch statement to add am or pm bellow as well as transform time to 12 hours formula
-	if (hour == 0) {
-		hour = hour + 12;
-		
-		if (minute < 10){
-			return hour + ":" + "0" + minute +" "+ "AM";
-		}
-		
-		else
-		return hour + ":" + minute +" "+ "AM";
 	}
-	
-	
-	
-	else if (hour >= 1 & hour <= 11) {
-		
-		if (hour < 10 & minute < 10){
-			return "0" + hour + ":" + "0" + minute +" "+ "AM";
-		}
-		
-		if (hour < 10 & !(minute < 10)){
-			return "0" + hour + ":" + minute +" "+ "AM";
-		}
-		
-		if (!(hour < 10) & minute < 10){
-			return hour + ":" + "0" + minute +" "+ "AM";
-		}
-		
-		return hour + ":" + minute +" "+ "AM";
-	}
-	
-	
-	
-	else if (hour == 12) {
-		
-		if (minute < 10){
-			return hour + ":" + "0" + minute +" "+ "PM";
-		}
-		
-		else
-		return hour + ":" + minute +" "+ "PM";
-	}
-	
-	
-	
-	else if (hour >= 13 & hour <= 23) {
-		hour = hour - 12;
-		
-		if (hour < 10 & minute < 10){
-			return "0" + hour + ":" + "0" + minute +" "+ "PM";
-		}
-		
-		if (hour < 10 & !(minute < 10)){
-			return "0" + hour + ":" + minute +" "+ "PM";
-		}
-		
-		if (!(hour < 10) & minute < 10){
-			return hour + ":" + "0" + minute +" "+ "PM";
-		}
-		
-		return hour + ":" + minute +" "+ "PM";
-	}
-	
-	 
-			
-	return hour + ":" + minute;
-}
+/**
+ * 
+ * @return time in hours
+ */
+	public int getHour() {
+		return hour;
 
-}
+	}
+/**
+ * 
+ * @return time in minutes
+ */
+	public int getMinute() {
+		return minute;
+	}
+/**
+ * add time to current time
+ * @param h time in hours
+ * @param m time in minutes
+ */
+	public void addTime(int h, int m) {
+
+	
+
+		minute = minute + m;
+		// Add minutes after that make sure if minute is bigger than 59 then hour + 1, and
+		// minutes gets reset to the 60 minutes formula
+		if (minute > 59) {
+			hour = hour + 1;
+			minute = minute - 60;
+		}
+		// Add hours, after that make sure if hour is bigger than 23 then reset to 0 and hour
+		// gets reset to the 24 hours formula
+		hour = hour + h;
+
+		if (hour > 23) {
+			hour = hour - 24;
+		}
+
+	}
+/**
+ * to substract time
+ * @param h time in hours
+ * @param m time in minutes
+ */
+	public void substractTime(int h, int m) {
+		hour = hour - h;
+		if (hour < 0) {
+			hour = hour + 24;
+		}
+
+		minute = minute - m;
+		if (minute < 0) {
+			hour = hour - 1;
+			minute = minute + 60;
+		}
+
+	}
+/**
+ * 
+ * @return String representation of time 
+ */
+	public String getCurrentTime() {
+
+		// turn time from 24 hours format to 12 hours format
+		if (hour == 0) {
+			hour = hour + 12;
+
+			if (minute < 10) {
+				return hour + ":" + "0" + minute + " " + "AM";
+			}
+
+			else
+				return hour + ":" + minute + " " + "AM";
+		}
+
+		else if (hour >= 1 & hour <= 11) {
+//check if hours or minutes are less than 10 thus adding a 0 to the left 
+			if (hour < 10 & minute < 10) {
+				return "0" + hour + ":" + "0" + minute + " " + "AM";
+			}
+			//check if hours or minutes are less than 10 thus adding a 0 to the left 
+
+			if (hour < 10 & !(minute < 10)) {
+				return "0" + hour + ":" + minute + " " + "AM";
+			}
+			//check if hours or minutes are less than 10 thus adding a 0 to the left 
+
+			if (!(hour < 10) & minute < 10) {
+				return hour + ":" + "0" + minute + " " + "AM";
+			}
+//if the time was from 0 to 11 change to am
+			return hour + ":" + minute + " " + "AM";
+		}
+
+		else if (hour == 12) {
+			//check if hours or minutes are less than 10 thus adding a 0 to the left 
+
+			if (minute < 10) {
+				return hour + ":" + "0" + minute + " " + "PM";
+			}
+
+			else
+				return hour + ":" + minute + " " + "PM";
+		}
+
+		else if (hour >= 13 & hour <= 23) {
+			hour = hour - 12;
+			//check if hours or minutes are less than 10 thus adding a 0 to the left 
+
+			if (hour < 10 & minute < 10) {
+				return "0" + hour + ":" + "0" + minute + " " + "PM";
+			}
+			//check if hours or minutes are less than 10 thus adding a 0 to the left 
+
+			if (hour < 10 & !(minute < 10)) {
+				return "0" + hour + ":" + minute + " " + "PM";
+			}
+			//check if hours or minutes are less than 10 thus adding a 0 to the left 
+
+			if (!(hour < 10) & minute < 10) {
+				return hour + ":" + "0" + minute + " " + "PM";
+			}
+//if time was from 12 to 23 subtract 12 and change to pm
+			return hour + ":" + minute + " " + "PM";
+		}
+
+		return hour + ":" + minute;
+	}
+
 }
